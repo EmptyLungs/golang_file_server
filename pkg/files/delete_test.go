f9f80a5e636dddded5070c8e1456cb43903d6528
Dmitry Pavlenko <dmitryrint@gmail.com>
2023-11-11 01:10:46 +0900

    add another test

--- pkg/files/delete_test.go
+++ pkg/files/delete_test.go
@@ -1,10 +1,12 @@
 package files
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 )
 
@@ -29,4 +31,19 @@ func TestFileManagerDelete(t *testing.T) {
 	if err = fileManager.Delete(filename); err != nil {
 		t.Fatalf(err.Error())
 	}
+}
+
+func TestFileManagerDeleteFileNotExist(t *testing.T) {
+	assert := assert.New(t)
+	tempDir := t.TempDir()
+
+	fs := os.DirFS(tempDir)
+	logger := zap.NewNop()
+	fileManager, err := NewFileManager(tempDir, fs, logger)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = fileManager.Delete("test.txt")
+	assert.Error(err)
+	assert.True(errors.Is(err, os.ErrNotExist))
 }
