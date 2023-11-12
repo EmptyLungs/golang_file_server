cb4c5a21fe0c00706d8a7c2ecb0eedf3b8c0db65
Dmitry Pavlenko <dmitryrint@gmail.com>
2023-11-12 22:46:52 +0900

    refactor tests

--- pkg/grpc/files/mock.go
+++ pkg/grpc/files/mock.go
@@ -1,10 +1,15 @@
 package grpc
 
 import (
+	"context"
 	"io"
+	"net"
 
 	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
 )
 
 type MockFileManager struct {
@@ -24,8 +29,35 @@ func (m *MockFileManager) List() ([]string, error) {
 	return args.Get(0).([]string), args.Error(1)
 }
 
-func NewMockServer() (*FileServer, *MockFileManager) {
+func NewMockServer() (FileServiceClient, *MockFileManager, func()) {
 	logger, _ := zap.NewDevelopment()
 	fm := &MockFileManager{}
-	return &FileServer{logger: logger, fm: fm}, fm
+	server := &FileServer{logger: logger, fm: fm}
+	lis := bufconn.Listen(1024 * 1024)
+
+	srv := grpc.NewServer()
+	RegisterFileServiceServer(srv, server)
+
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			logger.Fatal(err.Error())
+		}
+	}()
+
+	closer := func() {
+		if err := lis.Close(); err != nil {
+			logger.Fatal(err.Error())
+		}
+		srv.Stop()
+	}
+	dialer := func(context.Context, string) (net.Conn, error) {
+		return lis.Dial()
+	}
+
+	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(dialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	client := NewFileServiceClient(conn)
+	return client, fm, closer
 }
--- pkg/grpc/files/upload_test.go
+++ pkg/grpc/files/upload_test.go
@@ -2,46 +2,16 @@ package grpc
 
 import (
 	"context"
-	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/test/bufconn"
 )
 
-func TestUploadHandler(t *testing.T) {
+func TestUploadHandlerw(t *testing.T) {
 	assert := assert.New(t)
-	lis := bufconn.Listen(1024 * 1024)
-	t.Cleanup(func() {
-		lis.Close()
-	})
-	srv := grpc.NewServer()
-	t.Cleanup(func() {
-		srv.Stop()
-	})
-	service, fm := NewMockServer()
-
-	RegisterFileServiceServer(srv, service)
-	// todo: fix this somehow
-	go func() {
-		if err := srv.Serve(lis); err != nil {
-			t.Fatalf(err.Error())
-		}
-	}()
-
-	fm.On("Create").Return(nil).Once()
-
-	dialer := func(context.Context, string) (net.Conn, error) {
-		return lis.Dial()
-	}
-
-	conn, err := grpc.DialContext(context.Background(), "", grpc.WithContextDialer(dialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	client := NewFileServiceClient(conn)
+	client, mfs, closer := NewMockServer()
+	t.Cleanup(closer)
+	mfs.On("Create").Return(nil)
 	stream, err := client.Upload(context.Background())
 	if err != nil {
 		t.Fatalf(err.Error())
